84e61f9a03121c604587b05dc214b5ef67ac3ca5
takurou Matsuo <m.taku7@gmail.com>
2016-09-15 22:16:22 +0900

    new project

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 var express = require('express'),
     app = express();
+    post = require('./routes/post');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 
-app.use(bodyParser.json());
+
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use( methodOverride());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
 
-app.get('/',function(req, res){
-  res.render('index', {title: 'title'});
-})
-
-app.param('id',function(req, res, next, id){
-  var users = ['japan','korea','china'];
-  req.params.name = users[id];
-  next();
-})
-
-app.get('/new',function(req, res){
-  res.render('new');
-});
-
-app.post('/create',function(req, res){
-  res.send(req.body.name);
-});
-
+app.get('/',post.index);
+/*
+app.get('/posts/new',post.new);
+app.post('/posts/create',post.create);
+app.get('/posts/:id`',post.show);
+app.get('/posts/:id`',post.edit);
+app.put('/posts/:id',post.update);
+app.delete('/posts/:id',post.destroy);
+*/
 
 
 app.listen(3000);
